2ee180f5033b83a70d52b874a2fe13b3ca07b16d
aryatripathi11 <25114cn014@glbitm.ac.in>
2026-06-04 08:06:48 +0530

    day8 assignment

---
+++ Day 8/ques31.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ques31 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
