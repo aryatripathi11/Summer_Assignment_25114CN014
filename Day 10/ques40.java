7a48c36cfeadd00411dba018d82f1036dc46a7e2
aryatripathi11 <25114cn014@glbitm.ac.in>
2026-06-06 17:18:10 +0530

    day 10 assignment

---
+++ Day 10/ques40.java
@@ -0,0 +1,23 @@
+
+public class ques40 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch++);
+            }
+            ch-=2;
+            for(int j=1;j<i;j++){
+                System.out.print(ch--);
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    
+}
